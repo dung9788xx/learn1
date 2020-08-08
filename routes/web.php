72229832f0796b8e4845105541836b682f5a0f90
DungDemo <dungdemoictu001@gmail.com>
2020-08-08 11:09:31 +0700

    abc

--- routes/web.php
+++ routes/web.php
@@ -12,8 +12,15 @@
 */
 
 Route::get('/', function () {
-    $a=\App\User::find(1);
-        return json_decode($a->message);
+//    $a=collect([['id'=>120,'max'=>1000],['id'=>130,'max'=>1500]]);
+//    $remap=$a->keyBy('id');
+//
+//    $b=array(['id'=>120,'name'=>'ban1'],['id'=>1340,'name'=>'ban2']);
+//    foreach ($b as &$c){
+//      echo  $remap[$c['id']]['max'];
+//    }
+//    dd($b);
+    echo "abc";
 });
 Route::get('/abc', function () {
     $sendmesage= new \App\Services\ChatWorkService();
